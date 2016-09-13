7f5294b0f910c9c1b1776650c7f72e66f5c778eb
Chernenkova <elena.chernenkova@bk.ru>
2016-09-14 01:19:53 +0300

    Task 1

---
+++ str.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void GenerateString(int n, char* str);
+
+int main()
+{
+	int n = 0;
+	printf("n = ");
+	scanf("%d", &n);
+	char* str = (char*)malloc((1<<n)*sizeof(char));
+	GenerateString(n, str);
+	free(str);
+	return 0;
+}
+void GenerateString(int n, char* str)
+{
+	int i = 0;
+	str[0] = 'a';
+	for(i = 1; i < n; i++)
+	{
+		str[strlen(str)] = str[0] + i;
+		strncpy(str + strlen(str), str, strlen(str) - 1);
+		
+	}
+	printf("%s", str);
+}
