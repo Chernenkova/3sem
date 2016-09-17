7cc1aeab235f33d96b8d4181246ac16bde3b4a7e
VladMiryaha <vlad.miryaha@gmail.com>
2016-09-17 18:28:36 +0300

    ok

--- str.c
+++ str.c
@@ -9,11 +9,21 @@ int main()
 	int n = 0;
 	printf("n = ");
 	scanf("%d", &n);
+/*
+ * ставьте пробелы вокруг бинарных операторов
+ * char* str = (char*)malloc((1 << n) * sizeof(char));
+ */
 	char* str = (char*)malloc((1<<n)*sizeof(char));
 	GenerateString(n, str);
+/*
+ * если вы в функцию передаёте указатель на массив, куда надо записать результат, а ф-я
+ * называется Generete, а не GenerateAndPrint, то лучше вывести результат вне ф-и
+ */
+  printf("%s", str);
 	free(str);
 	return 0;
 }
+
 void GenerateString(int n, char* str)
 {
 	int i = 0;
@@ -22,7 +32,5 @@ void GenerateString(int n, char* str)
 	{
 		str[strlen(str)] = str[0] + i;
 		strncpy(str + strlen(str), str, strlen(str) - 1);
-		
 	}
-	printf("%s", str);
 }
--- strdel.c
+++ strdel.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
+
+/*
+ * Лучше называйте все define'ы заглавными буквами, а отдельные слова разделяйте пробелами:
+ * N
+ * MAX_STRING_SIZE
+ * 
+ * Также названия лучше выбирать "говорящими", т.к. не посмотрев дальше в код смысл переменной N понять сложно
+ */
 #define n 255
 
 void Split(char* string, char* delimiters, char*** tokens, int* tokensCount);
@@ -21,6 +29,10 @@ int main ()
 	for (i = 0; i < tokensCount; i++)
 		printf("%s\n",tokens[i]);
 	
+/*
+ * Если вы где-то выделили память, то её следует освободить самим с помощью free
+ */
+
 	return 0;
 }
 
