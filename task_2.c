5cdd3deee413fc905a5b95943706a96d725fedcb
VladMiryaha <vlad.miryaha@gmail.com>
2016-10-02 03:39:21 +0300

    задание не засчитано

--- task_2.c
+++ task_2.c
@@ -2,8 +2,11 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/types.h>
-#define NUMBER_OF_TREES 10
 
+/*
+ * number of nodes?
+ */
+#define NUMBER_OF_TREES 10
 
 int main()
 {
@@ -14,6 +17,12 @@ int main()
 	pid_t pid = fork();
 	if(pid != 0)
 	{
+/*
+ * FIXIT:
+ * когда ф-я принимает указатель на какие-то данные, то они вероятно выходные. как раз для wait статус завершения дочернего процесса записывается в участок памяти,
+ * но который ссылается переданный указатель. т.е. обязательно предполагается, что память под него выделена либо на стеке, либо в куче.
+ * у вас же status - некая неинициализированная переменная ...
+ */
 	    wait(status);
 	    printf("The child of %d has finished.\n", getppid());
     	    exit(0);
